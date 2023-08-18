213434655d59ef7148b08d8adc89916c5f4d6ec0
junai <junaidchohan@gmail.com>
2023-08-18 17:27:43 +0500

    This is my first pus for this branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:catalog_app/homePage.dart';
+import 'package:catalog_app/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'home_page.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +13,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (Context) => LoginPage(),
+        MyRoutes.homeRoute: (Context) => HomePage(),
+        MyRoutes.loginRoute: (Context) => LoginPage(),
+      },
     );
   }
 }
